f4f23837448bceda81bc7bc42958beca634bc620
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-04-21 16:41:36 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'Tristan',
+  author: '🥹❤️',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Tristan', // Your name
+  ownerNumber: '27634624586', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
